a091ffeeafee8301878d4c32e1903ba960c4b8fa
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-13 07:37:36 +0200

    exersise

--- JS Advanced/02. Exersise syntax,functions,statements/08. Words Uppercase.js
+++ JS Advanced/02. Exersise syntax,functions,statements/08. Words Uppercase.js
@@ -1,4 +1,8 @@
-function wordsUppercase() {
+function wordsUppercase(input) {
+    let reg = /\b[a-zA-Z0-9]+\b/g
+    let match = input.match(reg).map(x => x.toUpperCase())
+    return match.join(', ')
 
 }
-console.log(wordsUppercase());
+console.log(wordsUppercase('Hi, how are you?'));
+console.log(wordsUppercase('hello'));
