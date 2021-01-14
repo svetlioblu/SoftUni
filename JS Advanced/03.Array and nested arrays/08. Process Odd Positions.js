f80fa5406158d60584893372ce90db769dccce48
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-14 08:32:11 +0200

    exersise

--- JS Advanced/03.Array and nested arrays/08. Process Odd Positions.js
+++ JS Advanced/03.Array and nested arrays/08. Process Odd Positions.js
@@ -1,4 +1,6 @@
 function solve(arr) {
-
+    let filtered =arr.filter((x, i) => i % 2 != 0)
+    .map(x => x * 2).reverse()
+    return filtered
 }
-console.log(solve());
+console.log(solve([10, 15, 20, 25]));
