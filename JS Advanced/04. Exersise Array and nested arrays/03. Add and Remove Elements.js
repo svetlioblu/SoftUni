e59fd1c65ab9b7b950e7feb2b3c2992fec183c36
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-15 08:44:51 +0200

    exersise

---
+++ JS Advanced/04. Exersise Array and nested arrays/03. Add and Remove Elements.js
@@ -0,0 +1,40 @@
+function solve(arr) {
+    let result = []
+    let counter = 0
+    let command = {
+        add() {
+            counter += 1
+            result.push(counter)
+        },
+        remove() {
+            counter += 1
+            result.pop()
+        }
+    }
+
+    for (let line of arr) {
+        command[line]()
+    }
+    if (result.length === 0) {
+        return 'Empty'
+    } else {
+        return result.join('\n')
+    }
+}
+console.log(solve(['add',
+    'add',
+    'add',
+    'add']
+));
+console.log('-------------');
+console.log(solve(['add',
+    'add',
+    'remove',
+    'add',
+    'add']
+));
+console.log('-------------');
+console.log(solve(['remove',
+    'remove',
+    'remove']
+));
