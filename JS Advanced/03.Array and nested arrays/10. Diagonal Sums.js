ef5108f23f056e6c75a5d793763644ec7211b257
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-15 06:48:10 +0200

    exersise

--- JS Advanced/03.Array and nested arrays/10. Diagonal Sums.js
+++ JS Advanced/03.Array and nested arrays/10. Diagonal Sums.js
@@ -1,4 +1,18 @@
 function solve(matrix) {
-
+    let firstDiagonal = 0
+    let secondDiagonal = 0
+    for (let row = 0; row < matrix.length; row++) {
+        firstDiagonal += matrix[row][row];
+        secondDiagonal += matrix[row][matrix.length - row - 1]
+    }
+    return `${firstDiagonal} ${secondDiagonal}`
 }
-console.log(solve());
+console.log(solve([
+    [20, 40],
+    [10, 60]]
+));
+console.log(solve([[3, 5, 17],
+    [-1, 7, 14],
+    [1, -8, 89]]
+   
+));
