4b1b03dfff10eff03f23e5f48528cac23915c8b9
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-18 07:47:42 +0200

    exersise

--- JS Advanced/04. Exersise Array and nested arrays/07. Sorting Numbers.js
+++ JS Advanced/04. Exersise Array and nested arrays/07. Sorting Numbers.js
@@ -1,12 +1,13 @@
 function solve(arr) {
     let result = []
     let sorted = arr.sort((a, b) => a - b)
-    sorted.forEach((element, i) => {
+    let copySorted = sorted.slice()
+    sorted.forEach((_, i) => {
         if (i % 2 == 0) {
-            result.unshift(element)
-        } else { result.push(element) }
+            result.push(copySorted.shift())
+        } else { result.push(copySorted.pop()) }
     });
 
     return result
 }
-console.log(solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
+console.log(solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56, 15]));
