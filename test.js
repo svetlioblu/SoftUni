5fd5d15084a2d9fb16cc60033028ff7970b1a23c
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-19 06:53:21 +0200

    exersise

--- test.js
+++ test.js
@@ -6,35 +6,19 @@ let dashboard = [
 
 let player = 'X'
 
-function verifyIfWin(dashboard, player) {
-
-    let primary = []
-    let secondary = []
-    let middle = []
-    let firstRow = []
-    let lastRow = []
-    let firstCol = []
-    let middleCol = []
-    let lastCol = []
-    for (let i = 0; i < dashboard.length; i++) {
-        primary.push(dashboard[i][i])
-        secondary.push(dashboard[i][dashboard.length - i - 1])
-        firstRow.push(dashboard[0][i])
-        middle.push(dashboard[1][i])
-        lastRow.push(dashboard[2][i])
-        firstCol.push(dashboard[i][0])
-        middleCol.push(dashboard[i][1])
-        lastCol.push(dashboard[i][2])
-
+let department = {
+    name: "Engineering",
+    data: {
+        director: {
+            name: 'John',
+            position: 'Engineering Director'
+        },
+        employees: [],
+        company: 'Quick Build'
     }
-    if (primary.every(el => el === player) || secondary.every(el => el === player) || middle.every(el => el === player) || firstRow.every(el => el === player) || lastRow.every(el => el === player) || firstCol.every(el => el === player) || middleCol.every(el => el === player) || lastCol.every(el => el === player)) {
-        return true
-    } else {
-        return false
-    }
+};
 
-}
-
-
-
-console.log(verifyIfWin(dashboard, player))
+let {data:{director:{name}}} = department
+name = 'Ivan'
+console.log(name);
+console.log(department.data);
