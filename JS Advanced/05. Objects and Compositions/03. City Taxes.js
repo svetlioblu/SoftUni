ac389197e2367d883ba61f19c94290b673d84df3
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-19 08:08:40 +0200

    exersise

--- JS Advanced/05. Objects and Compositions/03. City Taxes.js
+++ JS Advanced/05. Objects and Compositions/03. City Taxes.js
@@ -1,4 +1,30 @@
-function solve(params) {
+function cityTaxes(name, population, treasury) {
+    let city = {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += this.population * this.taxRate
+            return this.treasury
+        },
+        applyGrowth(percentage) {
+            this.population += Math.floor(this.population * percentage / 100)
+            return this.population
+        },
+        applyRecession(percentage) {
+            this.treasury -= Math.floor(this.treasury * percentage / 100)
+            return this.treasury
+        }
 
+    }
+    return city
 }
-console.log(solve());
+
+const city = cityTaxes('Tortuga', 7000, 15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyRecession(5);
+console.log(city.population);
+
+
--- test.js
+++ test.js
@@ -1,24 +1,6 @@
-let dashboard = [
-    ['X', 'X', false],
-    ['X', 'X', 'X'],
-    ['Xd', 'X', false]
-]
+let obj = { a: 1, b: 2 }
+let copy ={}
 
-let player = 'X'
-
-let department = {
-    name: "Engineering",
-    data: {
-        director: {
-            name: 'John',
-            position: 'Engineering Director'
-        },
-        employees: [],
-        company: 'Quick Build'
-    }
-};
-
-let {data:{director:{name}}} = department
-name = 'Ivan'
-console.log(name);
-console.log(department.data);
+Object.assign(copy,obj)
+copy.a = 5
+console.log(obj);
