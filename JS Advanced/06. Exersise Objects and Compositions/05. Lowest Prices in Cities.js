1affbcf3c3e3fbf43b0bd5ce0372b9c5ad6a6c10
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-21 08:51:41 +0200

    exersise

--- JS Advanced/06. Exersise Objects and Compositions/05. Lowest Prices in Cities.js
+++ JS Advanced/06. Exersise Objects and Compositions/05. Lowest Prices in Cities.js
@@ -1,4 +1,30 @@
 function solve(params) {
-    
+    let obj = {}
+    for (let line of params) {
+        let [town, product, price] = line.split(' | ')
+        price = Number(price)
+        if (obj[product]) {
+            if (obj[product][0] > price) {
+                obj[product][0] = price
+                obj[product][1] = town
+            }
+        } else {
+
+            obj[product] = [price, town]
+        }
+    }
+    let result = []
+    for (let item in obj) {
+        result.push(`${item} -> ${obj[item][0]} (${obj[item][1]})`)
+    }
+    return result.join('\n')
 }
-console.log(solve());
+console.log(solve(['Sample Town | Sample Product | 1000',
+'Sample Town | Orange | 2',
+'Sample Town | Peach | 1',
+'Sofia | Orange | 3',
+'Sofia | Peach | 2',
+'New York | Sample Product | 1000.1',
+'New York | Burger | 10']
+
+));
