0792e2b0dbb7426915fd399240c268c8c098d976
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-22 08:56:23 +0200

    exersise

--- JS Advanced/07. DOM inroduction/05.Colorize Table/ColorizeTable.js
+++ JS Advanced/07. DOM inroduction/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,6 @@
 function colorize() {
-    // TODO
+    let node = Array.from(document.querySelectorAll('table tr:nth-child(2n+2)'))
+    node.forEach(el => {
+        el.style.backgroundColor = 'Teal'
+    })
 }
