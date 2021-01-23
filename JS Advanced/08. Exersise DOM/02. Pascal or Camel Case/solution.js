67c958dff12a60eb26c61ffd790c7ed5fceaff3a
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-23 16:19:39 +0200

    exersise

--- JS Advanced/08. Exersise DOM/02. Pascal or Camel Case/solution.js
+++ JS Advanced/08. Exersise DOM/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,26 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById(`text`).value
+  let caseType = document.getElementById('naming-convention').value
+  let result = document.getElementById('result')
+  let cases = {
+    'Camel Case': function (input) {
+      input = input.split(' ')
+      return input.map((x, i) => {
+        if (i > 0) {
+          return x[0].toUpperCase() + (x.substring(1).toLowerCase())
+        }
+        return x.toLowerCase()
+      }).join('')
+    },
+    'Pascal Case': function (input) {
+      return input.split(' ')
+        .map(x => x[0].toUpperCase() + (x.substring(1).toLowerCase())).join('')
+    }
+  }
+
+  if (!cases[caseType]) {
+    result.textContent = 'Error!'
+  } else {
+    result.textContent = cases[caseType](input)
+  }
 }
--- test.js
+++ test.js
@@ -1,9 +1,17 @@
-let str = 'The Rose Valley (Bulgaria) is located just south of the Balkan Mountains (Kazanlak).The most common oil-bearing rose found in the valley is the pink-petaled Damask rose (Rosa damascena Mill).'
-let reg = /\((.+?)\)/g
-    let result = []
-    let match = reg.exec(str)
-    while(match) {
-        result.push(match[1])
-        match = reg.exec(str)
+// let str = 'The Rose Valley (Bulgaria) is located just south of the Balkan Mountains (Kazanlak).The most common oil-bearing rose found in the valley is the pink-petaled Damask rose (Rosa damascena Mill).'
+// let reg = /\((.+?)\)/g
+//     let result = []
+//     let match = reg.exec(str)
+//     while(match) {
+//         result.push(match[1])
+//         match = reg.exec(str)
+//     }
+//     console.log(result);
+let arr = ['dd', 'dd','dd','dd']
+let b = arr.map((x, i) => {
+    if (i > 0) {
+        return x[0].toUpperCase() + x.substring(1)
     }
-    console.log(result);
+    return x
+})
+console.log(b);
