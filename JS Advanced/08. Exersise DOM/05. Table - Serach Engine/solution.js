bc41bfcb3ee8a3bb4b01a0d2ab4978438d7b9c10
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-24 09:36:42 +0200

    exersise

--- JS Advanced/08. Exersise DOM/05. Table - Serach Engine/solution.js
+++ JS Advanced/08. Exersise DOM/05. Table - Serach Engine/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-   //TODO...
+   document.querySelector('#searchBtn').addEventListener('click', onClick)
+   let input = document.getElementById('searchField')
+   let rows = document.querySelectorAll('tbody tr')
+   let isSearched = false
+   function onClick() {
+
+      if (isSearched) {
+         for (let row of rows) {
+            row.removeAttribute('class')
+            isSearched = false
+         }
+      }
+      for (let row of rows) {
+         if (input.value != '' && row.textContent.includes(input.value)) {
+            row.setAttribute('class', 'select')
+            isSearched = true
+         }
+      }
+   }
 }
--- JS Advanced/08. Exersise DOM/06. Format the Text/solution.js
+++ JS Advanced/08. Exersise DOM/06. Format the Text/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO
+  let input = document.getElementById('input')
+  let output = document.getElementById('output')
+
+  if (output.value != '') {
+    let tokens = input.value.split('.')
+      .filter(x => x != '')
+      .map(x => x.trim() + '.')
+
+    for (let i = 0; i < tokens.length; i++) {
+      if (i % 3 === 0) {
+        output.innerHTML += `<p>${tokens[i]}</p>`
+      } else {
+        output.lastChild.textContent += `${tokens[i]}`
+      }
+    }
+  }
 }
