b912d3083e86b57837b1b8fe424ad24222bb5af1
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-24 18:09:22 +0200

    exersise

--- JS Advanced/08. Exersise DOM/07. Hell's Kitchen/app.js
+++ JS Advanced/08. Exersise DOM/07. Hell's Kitchen/app.js
@@ -1,3 +1,42 @@
 function solve() {
-	//TODO
+	document.getElementById('btnSend').addEventListener('click', onClick)
+	let input = document.querySelector('#inputs textarea')
+	let bestRestaurantOutput = document.querySelector('#bestRestaurant p')
+	let bestWorkersOutput = document.querySelector('#workers p')
+
+	function onClick() {
+		
+		if (input.value != '') {
+	
+			let inputArr = JSON.parse(input.value)
+			let restaurants = {}
+			for (let line of inputArr) {
+				//["PizzaHut - Peter 500, George 300, Mark 800", "TheLake - Bob 1300, Joe 780, Jane 660"]
+				let [restaurant, ...workers] = line.split(' - ')
+				if (!restaurants[restaurant]) {
+					restaurants[restaurant] = {}
+				}
+				workers[0].split(', ').forEach(element => {
+					let [worker, salary] = element.split(' ')
+					salary = Number(salary)
+					restaurants[restaurant][worker] = salary
+				})
+			}
+			let avgBest = 0
+			let bestRestaurant = Object.entries(restaurants).sort(([nameA, paramsA], [nameB, paramsB]) => {
+				let sumA = Object.values(paramsA).reduce((a, b) => a + b) / Object.values(paramsA).length
+				let sumB = Object.values(paramsB).reduce((a, b) => a + b) / Object.values(paramsB).length
+				sumA > sumB ? avgBest = sumA : sumB
+				return sumB - sumA
+			})[0]
+			let bestWorkers = Object.entries(bestRestaurant[1]).sort((a, b) => b[1] - a[1])
+			bestRestaurantOutput.textContent = `Name: ${bestRestaurant[0]} Average Salary: ${avgBest.toFixed(2)} Best Salary: ${bestWorkers[0][1].toFixed(2)}`
+			bestWorkers.forEach(el => {
+				bestWorkersOutput.textContent += `Name: ${el[0]} With Salary: ${el[1]} `
+			})
+			
+		}
+	}
 }
+//["PizzaHut - Peter 500, George 300, Mark 800", "TheLake - Bob 1300, Joe 780, Jane 660", "Other - Peter1 500, George2 300, Mark3 800"]
+//["Other - Peter1 500, George2 300, Mark3 800", "PizzaHut - Peter 500, George 300, Mark 800", "TheLake - Bob 1300, Joe 780, Jane 660"]
--- JS Advanced/08. Exersise DOM/08. Generate Report/report.js
+++ JS Advanced/08. Exersise DOM/08. Generate Report/report.js
@@ -1,6 +1,5 @@
 function generateReport(colNames) {
     const checks = document.querySelectorAll('input[type="checkbox"]');
-    const data = [...document.querySelectorAll('tbody>tr')].map(r => [...r.children].reduce((acc, col, i) => Object.assign(acc, checks[i].checked ? {[checks[i].name]: col.textContent} : {}), {}));
-
+    const data = Array.from(document.querySelectorAll('tbody>tr')).map(r => Array.from(r.children).reduce((acc, col, i) => Object.assign(acc, checks[i].checked ? { [checks[i].name]: col.textContent } : {}), {}));
     document.getElementById('output').value = JSON.stringify(data, null, 2);
 }
