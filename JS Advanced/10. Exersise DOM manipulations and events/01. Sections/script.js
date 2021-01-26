43105b0e361764e77d6d8a17e05ac413e98c7767
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-26 11:55:56 +0200

    exersise

--- JS Advanced/10. Exersise DOM manipulations and events/01. Sections/script.js
+++ JS Advanced/10. Exersise DOM manipulations and events/01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+   let content = document.getElementById('content')
+   words.forEach(element => {
+      let currentDiv = document.createElement('div')
+      let currentP = document.createElement('p')
+      currentP.textContent = element
+      currentP.style.display = 'none'
+      currentDiv.appendChild(currentP)
+      content.appendChild(currentDiv)
+   })
+   Array.from(content.children).map(el => {
+      el.addEventListener('click', onClick)
+   })
+   function onClick(ev) {
+      ev.target.children[0].style.display=''
+   }
 }
