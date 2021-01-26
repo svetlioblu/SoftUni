4965ff61e344a35235fa1183415f253b5c8434f4
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-26 07:22:00 +0200

    exersise

--- JS Advanced/09. DOM manipulations and events/05. Highlight-Active/app.js
+++ JS Advanced/09. DOM manipulations and events/05. Highlight-Active/app.js
@@ -1,3 +1,13 @@
 function focus() {
-    console.log('TODO:...');
+    let fields = document.querySelectorAll('input[type="text"]')
+    for (let input of fields) {
+        input.addEventListener('focus', onFocus)
+        input.addEventListener('blur', onBlur)
+    }
+    function onFocus(ev) {
+        ev.target.parentNode.className = 'focused'
+    }
+    function onBlur(ev) {
+        ev.target.parentNode.className = ''
+    }
 }
