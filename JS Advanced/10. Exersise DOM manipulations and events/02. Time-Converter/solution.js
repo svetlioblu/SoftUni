118664047b10fd2ed3ded5cc00e453ee4d4b064e
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-26 16:10:06 +0200

    exersise

--- JS Advanced/10. Exersise DOM manipulations and events/02. Time-Converter/solution.js
+++ JS Advanced/10. Exersise DOM manipulations and events/02. Time-Converter/solution.js
@@ -1,4 +1,35 @@
 function attachEventsListeners() {
+    let main = document.querySelector('main')
+    let days = document.getElementById('days')
+    let hours = document.getElementById('hours')
+    let minutes = document.getElementById('minutes')
+    let seconds = document.getElementById('seconds')
+    main.addEventListener('click', onClick)
 
-    console.log('TODO:...');
+    function onClick(event) {
+        if (event.target.defaultValue == 'Convert') {
+
+            if (days.value != '') {
+                let current = Number(days.value)
+                hours.value = 24 * current
+                minutes.value = 1440 * current
+                seconds.value = 86400 * current
+            } else if (hours.value != '') {
+                let current = Number(hours.value)
+                days.value = current / 24
+                minutes.value = current * 60
+                seconds.value = current * 60 * 60
+            } else if (minutes.value != '') {
+                let current = Number(minutes.value)
+                days.value = (current / 60) / 24
+                hours.value = current / 60
+                seconds.value = current * 60
+            } else if (seconds.value != '') {
+                let current = Number(seconds.value)
+                days.value = (current / 60 / 60) / 24
+                hours.value = current / 60 / 60
+                minutes.value = current / 60
+            }
+        }
+    }
 }
