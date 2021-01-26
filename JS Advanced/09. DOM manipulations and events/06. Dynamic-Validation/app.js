dc4c55832393b417179f5216f6845e9b33b70e8e
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-26 07:34:29 +0200

    exersise

--- JS Advanced/09. DOM manipulations and events/06. Dynamic-Validation/app.js
+++ JS Advanced/09. DOM manipulations and events/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    let field = document.getElementById('email')
+    field.addEventListener('change', onChange)
+    function onChange(ev) {
+        let reg = /^[a-z]+@[a-z]+\.[a-z]+$/
+        let validate = (reg).test(ev.target.value)
+        if (!validate) {
+            ev.target.className = 'error'
+        } else {
+            ev.target.className = ''
+        }
+    }
 }
