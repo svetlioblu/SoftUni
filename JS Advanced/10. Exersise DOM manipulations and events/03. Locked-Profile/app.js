a9b595a2502b1a6fe5423f3dbe8d281885ae73f6
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-27 08:00:55 +0200

    exersise

--- JS Advanced/10. Exersise DOM manipulations and events/03. Locked-Profile/app.js
+++ JS Advanced/10. Exersise DOM manipulations and events/03. Locked-Profile/app.js
@@ -1,3 +1,18 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = Array.from(document.getElementsByTagName('button'))
+    buttons.forEach(el => el.addEventListener('click', onClick))
+
+    function onClick(event) {
+        let lock = event.target.parentNode.querySelector('input[value="lock"]')
+
+        if (!lock.checked) {
+            if (event.target.parentNode.querySelector('div').style.display == '') {
+                event.target.parentNode.querySelector('div').style.display = 'block'
+                event.target.textContent = 'Hide it'
+            } else {
+                event.target.parentNode.querySelector('div').style.display = ''
+                event.target.textContent = 'Show more'
+            }
+        }
+    }
 }
--- JS Advanced/10. Exersise DOM manipulations and events/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/10. Exersise DOM manipulations and events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    let dropDown = document.getElementById('menu')
+    let text = document.getElementById('newItemText').value
+    let value = document.getElementById('newItemValue').value
+
+    let option = document.createElement('option')
+    let attribute = document.createAttribute('value')
+    attribute.value = value
+    option.setAttributeNode(attribute)
+    option.textContent = text
+    dropDown.appendChild(option)
+    document.getElementById('newItemText').value= ''
+    document.getElementById('newItemValue').value = ''
 }
