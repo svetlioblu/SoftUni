86e173384b81a5563f716aea661cf8a6dd919240
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-27 08:26:44 +0200

    exersise

--- JS Advanced/10. Exersise DOM manipulations and events/05. Encode-and-Decode-Messages/app.js
+++ JS Advanced/10. Exersise DOM manipulations and events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,25 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let textAreaSend = document.querySelectorAll('textarea')[0]
+    let textAreaReceived = document.querySelectorAll('textarea')[1]
+    let buttonSend = document.getElementsByTagName('button')[0]
+    let buttonReceived = document.getElementsByTagName('button')[1]
+    let actions = {
+        encript: (value) => {
+            value = value.split('').map(x => String.fromCharCode(x.charCodeAt() + 1)).join('')
+            textAreaReceived.value = value
+            textAreaSend.value = ''
+        },
+        decript: (value) => {
+            value = value.split('').map(x => String.fromCharCode(x.charCodeAt() - 1)).join('')
+            textAreaReceived.value = value
+        }
+    }
+    buttonSend.addEventListener('click', () => {
+        if (textAreaSend.value != '') {
+            actions.encript(textAreaSend.value)
+        }
+    })
+    buttonReceived.addEventListener('click', () => {
+        actions.decript(textAreaReceived.value)
+    })
 }
