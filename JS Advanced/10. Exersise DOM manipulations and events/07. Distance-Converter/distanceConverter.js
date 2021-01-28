96dcd2cd7a688d297959592cc275b1fb9e2fbbc8
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-28 08:36:42 +0200

    exersise

--- JS Advanced/10. Exersise DOM manipulations and events/07. Distance-Converter/distanceConverter.js
+++ JS Advanced/10. Exersise DOM manipulations and events/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,26 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    document.querySelector('body').addEventListener('click', onClick)
+    let input = document.getElementById('inputDistance')
+    let output = document.getElementById('outputDistance')
+    let inputUnits = document.getElementById('inputUnits')
+    let outputUnits = document.getElementById('outputUnits')
+
+    function onClick(event) {
+        let toMeters = {
+            km: 1000,
+            m: 1,
+            cm: 0.01,
+            mm: 0.001,
+            mi: 1609.34,
+            yrd: 0.9144,
+            ft: 0.3048,
+            in: 0.0254
+        }
+        if (event.target.id == 'convert') {
+            output.value = Number(input.value) * toMeters[inputUnits.value] / toMeters[outputUnits.value]
+        }
+    }
 }
+// •	Multiply the incoming distance by the following conversion rates to convert to meter
+// •	Divide to convert from meters to the required output unit
+
--- JS Advanced/10. Exersise DOM manipulations and events/08. Sudomu/solution.js
+++ JS Advanced/10. Exersise DOM manipulations and events/08. Sudomu/solution.js
@@ -1,4 +1,35 @@
 function solve() {
+    let check = document.getElementsByTagName('button')[0]
+    let clear = document.getElementsByTagName('button')[1]
+    let cells = document.querySelectorAll('input[type="number"]')
+    check.addEventListener('click', onClickCheck)
 
-    //TODO...
+    function onClickCheck() {
+        let matrix = []
+        let temp = []
+
+        Array.from(cells).forEach((el, i) => {
+            if (el.value == '') {
+                //to do
+            } else if (i % Math.round(Math.sqrt(cells.length)) == 0 && i != 0) {
+                matrix.push(temp.slice())
+                temp = []
+            }
+            temp.push(Number(el.value))
+        })
+        matrix.push(temp)
+        for (let i = 1; i < matrix.length; i++) {
+            let firstRow = matrix[0].reduce((a, b) => a + b)
+            if (matrix[i].reduce((a, b) => a + b) != firstRow) {
+                //to do
+            }
+        }
+        let transMatrix = matrix[0].map((_, colindex) => matrix.map(row => row[colindex]))
+        for (let i = 1; i < transMatrix.length; i++) {
+            let firstRow = transMatrix[0].reduce((a, b) => a + b)
+            if (transMatrix[i].reduce((a, b) => a + b) != firstRow) {
+                //to do
+            }
+        } 
+    }
 }
