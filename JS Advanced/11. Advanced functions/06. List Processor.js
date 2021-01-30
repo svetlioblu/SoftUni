3d38febff45c1c5f5efdc57a558fe32610e8faec
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-01-30 13:46:26 +0200

    exersise

--- JS Advanced/11. Advanced functions/06. List Processor.js
+++ JS Advanced/11. Advanced functions/06. List Processor.js
@@ -1,3 +1,18 @@
-function name(params) {
-    
+function solve(input) {
+    let result = []
+    let actions = {
+        add: (str) => { result.push(str) },
+        remove: (str) => { result = result.filter(x => x != str) },
+        print: () => { console.log(result.join(',')) }
+    }
+    input.forEach(element => {
+        if (element == 'print') {
+            actions.print()
+        } else {
+            let [action, item] = element.split(' ')
+            actions[action](item)
+        }
+
+    })
 }
+solve(['add pesho', 'add george', 'add peter', 'remove peter','print'])
