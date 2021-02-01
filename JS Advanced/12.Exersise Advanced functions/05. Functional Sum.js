5d513a44da5b92f3fa276479c0a89727579df3d4
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-02-01 08:07:27 +0200

    exersise

--- JS Advanced/12.Exersise Advanced functions/05. Functional Sum.js
+++ JS Advanced/12.Exersise Advanced functions/05. Functional Sum.js
@@ -1,3 +1,12 @@
-function name(params) {
-    
+function add(num) {
+    let sum = 0
+    sum += num
+    calc.toString = () => { return sum }
+    return calc
+    function calc(num2) {
+        sum += num2
+        return calc
+    }
 }
+
+console.log(add(1)(6)(-3).toString());
