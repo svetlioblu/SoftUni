92f8d7e2dde833e6f1d15272368fd92b8438033e
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-02-04 17:27:30 +0200

    exersise

--- JS Advanced/14. Exersise Unit Testing/06.Dynamic-Validation/app.js
+++ JS Advanced/14. Exersise Unit Testing/06.Dynamic-Validation/app.js
@@ -1,3 +1,11 @@
 function validate() {
-    // TODO
+    let email = document.getElementById('email')
+    let reg = /^[a-z]+@[a-z]+\.[a-z]+$/gm
+    email.addEventListener('change', (e) => {
+        if (!reg.test(email.value)) {
+            email.setAttribute('class', 'error')
+        } else {
+            email.removeAttribute('class')
+        }
+    })
 }
