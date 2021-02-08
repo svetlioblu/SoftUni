7de0eca0b20c8ff02b3b10f32f9ec3efb64af7c4
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-02-08 16:35:19 +0200

    exersise

---
+++ JS Advanced/16. Exersise Classes/07. HEX.js
@@ -0,0 +1,26 @@
+class Hex {
+    constructor(value) {
+        this.value = value
+    }
+    valueOf() {
+        return this.value
+    }
+    toString() {
+        return '0x' + this.value.toString(16).toUpperCase()
+    }
+    plus(number) {
+        return new Hex(number + this.value)
+    }
+    minus(number) {
+        return new Hex( this.value-number)
+    }
+    parse (string) {
+        return  parseInt(string, 16)
+    }
+}
+let FF = new Hex(255);
+console.log(FF.toString());
+
+let a = new Hex(10);
+let b = new Hex(5);
+console.log(a.minus(b).toString());
