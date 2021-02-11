079a3bd9206b8d400e26e1a94d91b0404e64c737
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-02-11 16:27:22 +0200

    exersise

---
+++ JS Advanced/18. Exersise_Inheritance/03. Extensible Object.js
@@ -0,0 +1,25 @@
+function solve() {
+    let myObj = {}
+
+    myObj.extend = (value) => {
+        Object.entries(value).forEach(([key, value]) => {
+            if (typeof value === 'function') {
+                Object.getPrototypeOf(myObj)[key] = value
+            } else {
+                myObj[key] = value
+            }
+        })
+    }
+
+    return myObj
+}
+let a = solve()
+a.extend({
+    extensionProperty: 'someString',
+    extensionMethod: function () { },
+    extensionProperty2: 'someString2',
+}
+)
+let b = solve()
+
+console.log(Object.getPrototypeOf(b))
