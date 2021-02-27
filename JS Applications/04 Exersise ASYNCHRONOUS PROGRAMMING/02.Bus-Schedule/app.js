bf93089fc0a83d94005618d9c1449470c1b4a84e
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-02-27 13:56:10 +0200

    exersise

--- JS Applications/04 Exersise ASYNCHRONOUS PROGRAMMING/02.Bus-Schedule/app.js
+++ JS Applications/04 Exersise ASYNCHRONOUS PROGRAMMING/02.Bus-Schedule/app.js
@@ -1,11 +1,34 @@
 function solve() {
+    const info = document.getElementById('info')
+    const departBtn = document.getElementById('depart')
+    const arriveBtn = document.getElementById('arrive')
+    let currentStop = ''
+    let nextStop = {
+        name: 'depot',
+        next: '0361'
+    }
 
-    function depart() {
-        console.log('Depart TODO...');
+    async function depart() {
+        try {
+            let url = `http://localhost:3030/jsonstore/bus/schedule`
+            const responce = await fetch(url)
+            const data = await responce.json()
+            info.textContent = `Next stop ${nextStop.name}`
+            currentStop = nextStop.name
+            nextStop = data[nextStop.next]
+            departBtn.disabled = true
+            arriveBtn.disabled = false
+        } catch (error) {
+            info.textContent = `Error`
+            departBtn.disabled = true
+            arriveBtn.disabled = true
+        }
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        info.textContent = `Arriving at ${currentStop}`
+        departBtn.disabled = false
+        arriveBtn.disabled = true
     }
 
     return {
