fb9af99bc65279a1b797d9a42bdff47d324cfa1d
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-03-01 13:30:26 +0200

    exersise

--- JS Applications/04 Exersise ASYNCHRONOUS PROGRAMMING/05. Accordion/accordion.js
+++ JS Applications/04 Exersise ASYNCHRONOUS PROGRAMMING/05. Accordion/accordion.js
@@ -1,24 +1,20 @@
 async function solution() {
     await createElements()
-    //await getContent()
-
 }
 async function createElements() {
-    let ids = []
     const url = `http://localhost:3030/jsonstore/advanced/articles/list`
     const response = await fetch(url)
     const data = await response.json()
-    data.forEach(el => {
+    data.forEach(async (el) => {
         let currentElemenet = e('div', { class: 'accordion' }, '', e('div', { class: 'head' }, '', e('span', {}, `${el.title}`), e('button', { class: 'button', id: `${el._id}` }, 'More')))
         document.getElementById('main').appendChild(currentElemenet)
-        ids.push(el._id)
+
+        let getDetails = await getContent(el._id)
+        let details = e('div', { class: 'extra' }, '', e('p', {}, `${getDetails.content}`))
+        currentElemenet.appendChild(details)
+        document.getElementById(`${el._id}`).addEventListener('click', onClick)
     })
-    ids.forEach(e => {
-        let content = getContent(e)
-        console.log(content);
-    })
-    // let details = e('div', { class: 'extra' }, '', e('p', {}, `${extendDetails.content}`))
-    // currentElemenet.appendChild(details)
+
 }
 async function getContent(id) {
     let url = `http://localhost:3030/jsonstore/advanced/articles/details/${id}`
@@ -27,6 +23,17 @@ async function getContent(id) {
     return data
 }
 window.addEventListener('load', solution)
+function onClick(e) {
+    let currentEl = e.target.parentNode.parentNode.children[1]
+    
+    if (currentEl.style.display == '' || currentEl.style.display == 'none') {
+        e.target.parentNode.parentNode.children[1].style.display = 'block'
+        e.target.textContent = 'Less'
+    } else {
+        currentEl.style.display = 'none'
+        e.target.textContent = 'More'
+    }
+}
 function e(type, attribute, text, ...params) {
     let element = document.createElement(type)
     if (attribute != {} && attribute != undefined) {
