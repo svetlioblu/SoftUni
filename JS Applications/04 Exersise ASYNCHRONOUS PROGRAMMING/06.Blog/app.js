946812d3f2b893d8cf864ac0463914888ad0d74b
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-03-02 09:06:31 +0200

    exersise

--- JS Applications/04 Exersise ASYNCHRONOUS PROGRAMMING/06.Blog/app.js
+++ JS Applications/04 Exersise ASYNCHRONOUS PROGRAMMING/06.Blog/app.js
@@ -1,26 +1,50 @@
 function attachEvents() {
     document.getElementById('btnLoadPosts').addEventListener('click', loadPosts)
+    document.getElementById(`btnViewPost`).addEventListener('click', viewPosts)
 }
-attachEvents();
+attachEvents()
 
 async function loadPosts() {
+    const select = document.getElementById('posts')
 
-        const select = document.getElementById('posts')
-        const url = `http://localhost:3030/jsonstore/blog/posts`
-        const response = await fetch(url)
-        if(response.ok==false || response.status != 200) {
-            document.getElementById('post-body').textContent='Error'
-            throw new Error('Error')
-        }
-        const data = await response.json()
-    Object.entries(data).forEach(post => {
-        let addOption = e('option', { value: `${post[0]}` }, `${post[1].title}`)
-        select.appendChild(addOption)
-    })
+    const url = `http://localhost:3030/jsonstore/blog/posts`
+    const response = await fetch(url)
+    if (response.ok == false || response.status != 200) {
+        document.getElementById('post-body').textContent = 'Error'
+        throw new Error('Error')
+    }
+    const data = await response.json()
+    select.innerHTML = ''
+    if (document.getElementById('posts').children.length == 0) {
+        Object.entries(data).forEach(post => {
+            let addOption = e('option', { value: `${post[0]}` }, `${post[1].title}`)
+            select.appendChild(addOption)
+
+        })
+    }
+    return data
 }
+async function viewPosts() {
+    if (document.getElementById('posts').children.length > 0) {
+        const postId = document.getElementById('posts').value
+        const postTitle = document.getElementById('post-title')
+        const postBody = document.getElementById('post-body')
+        const postComments = document.getElementById('post-comments')
+        const url = `http://localhost:3030/jsonstore/blog/comments`
+        const response = await fetch(url)
+        const data = await response.json()
+        postComments.innerHTML = ''
+        let allPosts = await loadPosts()
+        let comments = Object.values(data).filter(x => x.postId == postId)
+        postTitle.textContent = allPosts[postId].title
+        postBody.textContent = allPosts[postId].body
+        comments.forEach(el => {
+            let comment = e('li', { id: `${el.id}` }, `${el.text}`)
+            postComments.appendChild(comment)
+        })
 
-
-
+    }
+}
 
 function e(type, attribute, text, ...params) {
     let element = document.createElement(type)
@@ -39,9 +63,3 @@ function e(type, attribute, text, ...params) {
     }
     return element
 }
-
-// postkey {
-//     body: {postBody},
-//     id: {postId},
-//     title: {postTitle} 
-//   }
