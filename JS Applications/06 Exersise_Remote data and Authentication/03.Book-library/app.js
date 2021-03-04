8b10f9c01d55dd0c57caeaaa76821b54a341e88e
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-03-04 16:53:18 +0200

    exersise

--- JS Applications/06 Exersise_Remote data and Authentication/03.Book-library/app.js
+++ JS Applications/06 Exersise_Remote data and Authentication/03.Book-library/app.js
@@ -1,4 +1,5 @@
 (function () {
+
     document.getElementsByTagName('form')[0].addEventListener('submit', postBook)
     document.getElementById('loadBooks').addEventListener('click', getBooks)
 
@@ -10,46 +11,49 @@ async function getBooks() {
     if (response.ok) {
         const data = await response.json()
         table.innerHTML = ''
-        Object.values(data).forEach(el => {
-            const addBook = e('tr', {}, '', e('td', {}, `${el.title}`), e('td', {}, `${el.author}`), e('td', {}, ``, e('button', { class: 'edit' }, `Edit`), e('button', { class: 'delete' }, `Delete`)))
+        Object.entries(data).forEach(el => {
+            const addBook = e('tr', {}, '', e('td', {}, `${el[1].title}`), e('td', {}, `${el[1].author}`), e('td', {}, ``, e('button', { class: 'edit' }, `Edit`), e('button', { class: 'delete' }, `Delete`)))
+            addBook.querySelector('.delete').addEventListener('click', (ev) => deleteBook(ev, el[0]))
+            addBook.querySelector('.edit').addEventListener('click', (ev) => editBook(ev, el[0]))
+
             table.appendChild(addBook)
         })
-        table.addEventListener('click', dispatcher)
     } else { alert(response.statusText) }
-
 }
 async function postBook(ev) {
     ev.preventDefault()
-    const title = document.getElementById('title')
-    const author = document.getElementById('author')
-    if (title.value == '' || author.value == '') {
-        throw new Error('Invalid Input!')
-    }
-    const url = 'http://localhost:3030/jsonstore/collections/books'
-    const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ author: author.value, title: title.value })
-    }
-    const response = await fetch(url, options)
-    // const data = await response.json()
-    // console.log(data);
-    author.value = ''
-    title.value = ''
-    if (!response.ok) {
-        alert(response.statusText)
-    }
+    console.log(document.querySelector('form button').textContent);
+    if (document.querySelector('form button').textContent == 'Submit') {
+        const title = document.getElementById('title')
+        const author = document.getElementById('author')
+        if (title.value == '' || author.value == '') {
+            throw new Error('Invalid Input!')
+        }
 
+        const url =  'http://localhost:3030/jsonstore/collections/books'
+        const method = 'POST'
+        const options = {
+            method,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ author: author.value, title: title.value })
+        }
+        const response = await fetch(url, options)
+        author.value = ''
+        title.value = ''
+        if (!response.ok) {
+            alert(response.statusText)
+        }
+    }
 }
-async function deleteBook() {
-
+async function editBook(ev, id) {
+    document.getElementsByTagName('h3')[0].textContent = 'Edit FORM'
+    document.querySelector('form button').textContent = 'Save'
 }
-function dispatcher(ev) {
-    if (ev.target.className == 'edit') {
+async function deleteBook(ev, id) {
+    const url = 'http://localhost:3030/jsonstore/collections/books/' + id
+    ev.target.parentNode.parentNode.remove()
+    await fetch(url, { method: 'DELETE' })
 
-    } else if (ev.target.className == 'dlete') {
-
-    }
 }
 
 function e(type, attribute, text, ...params) {
