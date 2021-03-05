62f7bfd577343f74ad074422a25894c93ac3f8a6
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-03-05 07:59:43 +0200

    exersise

--- JS Applications/06 Exersise_Remote data and Authentication/03.Book-library/app.js
+++ JS Applications/06 Exersise_Remote data and Authentication/03.Book-library/app.js
@@ -2,7 +2,7 @@
 
     document.getElementsByTagName('form')[0].addEventListener('submit', postBook)
     document.getElementById('loadBooks').addEventListener('click', getBooks)
-
+    document.querySelector('form button').className ='post'
 }())
 async function getBooks() {
     const table = document.getElementsByTagName('tbody')[0]
@@ -15,15 +15,14 @@ async function getBooks() {
             const addBook = e('tr', {}, '', e('td', {}, `${el[1].title}`), e('td', {}, `${el[1].author}`), e('td', {}, ``, e('button', { class: 'edit' }, `Edit`), e('button', { class: 'delete' }, `Delete`)))
             addBook.querySelector('.delete').addEventListener('click', (ev) => deleteBook(ev, el[0]))
             addBook.querySelector('.edit').addEventListener('click', (ev) => editBook(ev, el[0]))
-
             table.appendChild(addBook)
         })
     } else { alert(response.statusText) }
 }
 async function postBook(ev) {
     ev.preventDefault()
-    console.log(document.querySelector('form button').textContent);
-    if (document.querySelector('form button').textContent == 'Submit') {
+    ev.stopImmediatePropagation()
+    if (document.querySelector('form button').classList.contains('post')) {
         const title = document.getElementById('title')
         const author = document.getElementById('author')
         if (title.value == '' || author.value == '') {
@@ -38,22 +37,53 @@ async function postBook(ev) {
             body: JSON.stringify({ author: author.value, title: title.value })
         }
         const response = await fetch(url, options)
+        console.log('did post');
         author.value = ''
         title.value = ''
         if (!response.ok) {
             alert(response.statusText)
         }
-    } else {
-        const params = await editBook()
-        console.log(params);
     }
+
 }
-async function editBook(ev, id) {
-   
+async function editBook(event, id) {
+    const button = document.querySelector('form button')
     document.getElementsByTagName('h3')[0].textContent = 'Edit FORM'
     document.querySelector('form button').textContent = 'Save'
-    return id
+    button.classList.remove("post");
+    button.classList.add("edit");
+    if (button.classList.contains('edit')) {
+        button.addEventListener('click', (ev) => edit(event, id))
+    }
+    async function edit(ev, id) {
+        const title = document.getElementById('title')
+        const author = document.getElementById('author')
+        if (title.value == '' || author.value == '') {
+            throw new Error('Invalid Input!')
+        }
+
+        const url = 'http://localhost:3030/jsonstore/collections/books/' + id
+        const options = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ author: author.value, title: title.value })
+        }
+        const response = await fetch(url, options)
+        author.value = ''
+        title.value = ''
+        document.getElementsByTagName('h3')[0].textContent = 'FORM'
+        button.textContent = 'Submit'
+        button.classList.remove("edit");
+        button.classList.add("post");
+        console.log('did edit');
+        if (!response.ok) {
+            alert(response.statusText)
+        }
+    }
 }
+
+
+
 async function deleteBook(ev, id) {
     const url = 'http://localhost:3030/jsonstore/collections/books/' + id
     ev.target.parentNode.parentNode.remove()
