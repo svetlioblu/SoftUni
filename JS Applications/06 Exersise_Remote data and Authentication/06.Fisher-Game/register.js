d524ed3a314071be3961e8575f89614ef07be5aa
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-03-07 16:40:52 +0200

    exersise

---
+++ JS Applications/06 Exersise_Remote data and Authentication/06.Fisher-Game/register.js
@@ -0,0 +1,8 @@
+
+(function () {
+    document.getElementById('register').addEventListener('submit', (ev) => postRegister(ev, data))
+}())
+
+function postRegister(ev, data) {
+ev.preventDefault()
+}
