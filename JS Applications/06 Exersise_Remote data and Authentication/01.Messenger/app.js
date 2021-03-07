2910723829a8cc3e5f691122dc2740326253df3f
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-03-07 08:40:04 +0200

    exersise

--- JS Applications/06 Exersise_Remote data and Authentication/01.Messenger/app.js
+++ JS Applications/06 Exersise_Remote data and Authentication/01.Messenger/app.js
@@ -1,37 +1,40 @@
 function attachEvents() {
     document.getElementById('submit').addEventListener('click', send)
     document.getElementById('refresh').addEventListener('click', refresh)
-
+refresh()
 }
 attachEvents();
 
-async function send() {
-    const author = document.getElementById('author').value
-    const content = document.getElementById('content').value
-    if (author == '' || content == '') {
-        alert('Fields cannot be empty')
-        throw new Error('Fields cannot be empty')
-    }
-    let options = {
-        method: 'post',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ author, content })
-    }
-    const response = await fetch('http://localhost:3030/jsonstore/messenger', options)
-    if (!response.ok) {
-        alert(response.statusText)
-    }
 
-}
 async function refresh() {
     const responce = await fetch('http://localhost:3030/jsonstore/messenger')
     if (responce.ok) {
         const textArea = document.getElementById('messages')
         const data = await responce.json()
+        textArea.textContent = ''
         Object.values(data).forEach(msg => {
             textArea.textContent += `${msg.author}: ${msg.content}\n`
         })
     } else {
         alert(responce.statusText)
     }
+}
+async function send() {
+    const author = document.getElementById('author')
+    const content = document.getElementById('content')
+    if (author.value == '' || content.value == '') {
+        return alert('Fields cannot be empty')
+    }
+    let options = {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ author: author.value, content: content.value })
+    }
+    const response = await fetch('http://localhost:3030/jsonstore/messenger', options)
+    author.value = ''
+    content.value = ''
+    if (!response.ok) {
+        alert(response.statusText)
+    }
+
 }
--- JS Applications/06 Exersise_Remote data and Authentication/06.Fisher-Game/app.js
+++ JS Applications/06 Exersise_Remote data and Authentication/06.Fisher-Game/app.js
@@ -1,6 +1,48 @@
-function attachEvents() {
-    console.log('TODO...');
+async function attachEvents() {
+    document.querySelector('button.load').addEventListener('click', getCatches)
+}
+attachEvents()
+
+async function getCatches() {
+    const url = 'http://localhost:3030/data/catches'
+    const response = await fetch(url)
+    if (response.ok==false) {
+        let error = await response.json()
+        return alert(error.message)
+    }
+    const data = await response.json()
+    let result = data.map(createElements)
+    document.getElementById('main').innerHTML=`
+    <legend>Catches</legend>
+    ${result.join('\n')}
+    `
+   
 }
 
-attachEvents();
+
+function createElements(element) {
+    element = `<div class="catch">
+ <label>Angler</label>
+ <input type="text" class="angler" value="${element.angler}" />
+ <hr>
+ <label>Weight</label>
+ <input type="number" class="weight" value="${element.weight}" />
+ <hr>
+ <label>Species</label>
+ <input type="text" class="species" value="${element.species}" />
+ <hr>
+ <label>Location</label>
+ <input type="text" class="location" value="${element.location}" />
+ <hr>
+ <label>Bait</label>
+ <input type="text" class="bait" value="${element.bait}" />
+ <hr>
+ <label>Capture Time</label>
+ <input type="number" class="captureTime" value="${element.captureTime}" />
+ <hr>
+ <button disabled class="update">Update</button>
+ <button disabled class="delete">Delete</button>
+</div>`
+    return element
+}
 
