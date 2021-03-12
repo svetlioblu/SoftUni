97fac75d639e5215f0be9e97301133d1303772dd
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-03-12 06:43:42 +0200

    exersise

--- JS Applications/08 Exersise_Single Page Applications/02.Movies/src/app.js
+++ JS Applications/08 Exersise_Single Page Applications/02.Movies/src/app.js
@@ -8,18 +8,8 @@ const navigation = document.querySelector('nav')
 const registerForm = document.querySelector('#form-sign-up')
 const logInSection = document.getElementById('form-login')
 navigation.addEventListener('click', navClick)
-
-
-let email = sessionStorage.getItem('Email')
-if (email) {
-    Array.from(document.getElementsByClassName('guest')).forEach(e => e.style.display = 'none')
-    Array.from(document.getElementsByClassName('logged')).forEach(e => e.style.display = 'block')
-    document.getElementById('home').textContent = `Welcome, ${email}`
-    document.getElementById('add-movie-button').style.display = 'block'
-} else {
-    Array.from(document.getElementsByClassName('logged')).forEach(e => e.style.display = 'none')
-    Array.from(document.getElementsByClassName('guest')).forEach(e => e.style.display = 'block')
-    document.getElementById('add-movie-button').style.display = 'none'
+if (sessionStorage.getItem('Email')) {
+    document.getElementById('addBtn').addEventListener('click'.onAddMovie)
 }
 
 setUpHome(main, homepage)
