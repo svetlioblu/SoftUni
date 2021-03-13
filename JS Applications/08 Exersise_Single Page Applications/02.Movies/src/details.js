f00d1afe6b065801e591732c8cfaf49125a016e0
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-03-13 08:32:03 +0200

    exersise

---
+++ JS Applications/08 Exersise_Single Page Applications/02.Movies/src/details.js
@@ -0,0 +1,12 @@
+import { html, render } from 'https://unpkg.com/lit-html?module'
+let main;
+let section;
+
+export function setDetails(mainTarget, sectionTarget) {
+    main = mainTarget
+    section = sectionTarget
+}
+
+export function showDetails() {
+
+}
