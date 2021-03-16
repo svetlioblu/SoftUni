9a2e34702086ced847a3cae22d28a887ea8af6c7
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-03-16 07:32:09 +0200

    exersise

--- JS Applications/12 Exersose Client side Rendering/03.Search-in-List/search.js
+++ JS Applications/12 Exersose Client side Rendering/03.Search-in-List/search.js
@@ -1,3 +1,40 @@
+import { towns } from './towns.js'
+import { html, render } from 'https://unpkg.com/lit-html?module'
+
+const target = document.querySelector('body')
+let townsTemplate = (town) => html`
+<article>
+   <div id="towns">
+   </div>
+   <input type="text" id="searchText" />
+   <button @click=${search}>Search</button>
+   <div id="result">
+      <ul>
+         ${town.map(x => html`<li>${x}</li>`)}
+      </ul>
+      <span></span>
+   </div>
+</article>`
+
+update()
+const searchInput = document.getElementById('searchText')
+function update() {
+   render(townsTemplate(towns), target)
+}
 function search() {
-   // TODO
+   let match = 0
+   if (searchInput.value != '') {
+      Array.from(document.querySelectorAll('ul li')).map(x => {
+         if (x.textContent.toLowerCase().includes(searchInput.value.toLowerCase())) {
+            x.className = 'active'
+            match += 1
+            return x
+         } else {
+            x.className = ''
+            return x
+         }
+      })
+      searchInput.value = ''
+   }
+   document.querySelector('span').textContent = `${match} matches found`
 }
