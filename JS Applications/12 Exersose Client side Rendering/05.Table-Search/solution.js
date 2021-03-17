c9765fa8035dbd2d7fbf8e8c27178e801fb3ec31
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-03-17 09:41:00 +0200

    exersise

--- JS Applications/12 Exersose Client side Rendering/05.Table-Search/solution.js
+++ JS Applications/12 Exersose Client side Rendering/05.Table-Search/solution.js
@@ -1,8 +1,45 @@
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+import { html, render } from 'https://unpkg.com/lit-html?module'
+const searchBtn = document.querySelector('#searchBtn')
+const tbody = document.querySelector('tbody')
+const searchField = document.querySelector('#searchField')
 
-   function onClick() {
-      //   TODO:
 
+let template = (data) => html`
+${data.map(x => {
+   return html` <tr>
+   <td>${x.firstName} ${x.lastName}</td>
+   <td>${x.email}</td>
+   <td>${x.course}</td>
+</tr>`})}
+`
+update()
+searchBtn.addEventListener('click', () => {
+   if (searchField.value == '') {
+      return alert('The field is empty!')
+   }
+   Array.from(document.querySelectorAll('tbody tr')).forEach(x => {
+      x.removeAttribute('class')
+      if (x.textContent.toLowerCase().includes(searchField.value.toLowerCase())) {
+         x.setAttribute('class', 'select')
+         console.log(x);
+         searchField.value = ''
+      } 
+   })
+})
+
+async function update() {
+   let getRows = await getContent()
+   render(template(getRows), tbody)
+}
+
+async function getContent() {
+   const url = 'http://localhost:3030/jsonstore/advanced/table'
+   const response = await fetch(url)
+   if (response.ok) {
+      const data = await response.json()
+      return Object.values(data)
+   } else {
+      alert(response.statusText)
    }
 }
+window.getContent = getContent
