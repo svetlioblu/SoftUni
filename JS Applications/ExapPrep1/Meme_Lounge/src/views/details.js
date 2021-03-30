9482615bf05ce3f5667b552ec4f4b298627376d3
Svetoslav Genchev <svetlioblu@yahoo.com>
2021-03-30 07:55:38 +0300

    exeamPrep

--- JS Applications/ExapPrep1/Meme_Lounge/src/views/details.js
+++ JS Applications/ExapPrep1/Meme_Lounge/src/views/details.js
@@ -3,20 +3,18 @@ import { del, getOne } from '../data/api.js'
 
 let detailsPageTemplate = (data, isOwner, onDelete) => html`
 <section id="meme-details">
-    <h1>Meme Title:${data.title}
-
-    </h1>
+    <h1>Meme Title: ${data.title}</h1>
     <div class="meme-details">
         <div class="meme-img">
             <img alt="meme-alt" src="${data.imageUrl}">
         </div>
         <div class="meme-description">
             <h2>Meme Description</h2>
-            <p>
-                ${data.description}
-            </p>
-            ${(isOwner ? html` <a class="button warning" href="/edit/${data._id}">Edit</a>
-            <button @click=${onDelete} class="button danger">Delete</button>` : '')}
+            <p>${data.description}</p>
+            
+            ${isOwner ? html`
+            <a class="button warning" href="/edit/${data._id}">Edit</a>
+            <button @click=${onDelete} class="button danger">Delete</button>` : ''}
         </div>
     </div>
 </section>
@@ -26,10 +24,14 @@ let detailsPageTemplate = (data, isOwner, onDelete) => html`
 export async function deteilsPage(ctx) {
     let id = ctx.params.id
     let loggedOwner = sessionStorage.getItem('userId')
-    let data = await getOne(`http://localhost:3030/data/memes/` + id)
-    ctx.render(detailsPageTemplate(data, loggedOwner == data._ownerId, onDelete))
+    let data = await getOne(`http://localhost:3030/data/memes/${id}`)
+    const isOwner = loggedOwner === data._ownerId
+    ctx.render(detailsPageTemplate(data, isOwner, onDelete))
     async function onDelete() {
-        await del(`http://localhost:3030/data/memes/${id}`)
-        ctx.page.redirect('/All Memes')
+        let confirmed = confirm('Are you sure?')
+        if (confirmed) {
+            await del(`http://localhost:3030/data/memes/${id}`)
+            ctx.page.redirect('/All Memes')
+        }
     }
 }
