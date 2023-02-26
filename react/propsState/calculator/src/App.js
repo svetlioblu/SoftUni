95c25dbf74c32c27db99f0d1f12ff45a36b8693b
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-02-26 22:45:51 +0200

    ss

--- react/propsState/calculator/src/App.js
+++ react/propsState/calculator/src/App.js
@@ -1,32 +1,43 @@
 import Button from "./components/Button";
+import { useState } from "react"
 
 function App() {
- function Sum() {
-  console.log('ok');
- }
+  let [result, setResult] = useState('0')
+  function outputHandler(e) {
+    let input = e.target.innerHTML
+    switch (input) {
+      case '+':
+        
+        break;
+      default:
+
+    }
+    setResult(state => state + input)
+  }
   return (
     <div className="calculator">
-      <div className="calculator__output">0</div>
+      <div className="calculator__output">{result}</div>
       <div className="calculator__keys">
-        <Button operator='+'/>
-        <Button operator='-' />
-        <Button operator='x' />
-        <Button operator='÷' />
-        <Button operator='7' />
-        <Button operator='8' />
-        <Button operator='9' />
-        <Button operator='4' />
-        <Button operator='5' />
-        <Button operator='6' />
-        <Button operator='1' />
-        <Button operator='2' />
-        <Button operator='3' />
-        <Button operator='0' />
-        <Button operator='.' />
-        <Button operator='AC' />
-        <Button operator='=' />
+        <Button operator='+' outputHandler={outputHandler} />
+        <Button operator='-' outputHandler={outputHandler} />
+        <Button operator='x' outputHandler={outputHandler} />
+        <Button operator='÷' outputHandler={outputHandler} />
+        <Button operator='7' outputHandler={outputHandler} />
+        <Button operator='8' outputHandler={outputHandler} />
+        <Button operator='9' outputHandler={outputHandler} />
+        <Button operator='4' outputHandler={outputHandler} />
+        <Button operator='5' outputHandler={outputHandler} />
+        <Button operator='6' outputHandler={outputHandler} />
+        <Button operator='1' outputHandler={outputHandler} />
+        <Button operator='2' outputHandler={outputHandler} />
+        <Button operator='3' outputHandler={outputHandler} />
+        <Button operator='0' outputHandler={outputHandler} />
+        <Button operator='.' outputHandler={outputHandler} />
+        <Button operator='AC' outputHandler={outputHandler} />
+        <Button operator='=' outputHandler={outputHandler} />
       </div>
     </div>
+
   );
 }
 
--- react/propsState/calculator/src/components/Button.js
+++ react/propsState/calculator/src/components/Button.js
@@ -1,17 +1,18 @@
 
-let Button = (props) => {
+let Button = ({ operator, outputHandler }) => {
     //let operators = ['+', '-', 'x', '÷', '7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.', 'AC', '=']
     let addClass = () => {
-        if (props.operator === '+' || props.operator === '-' || props.operator === '÷' || props.operator === 'x') {
+        if (operator === '+' || operator === '-' || operator === '÷' || operator === 'x') {
             return ' calculator__key--operator'
-        } else if (props.operator === '=') {
+        } else if (operator === '=') {
             return ' calculator__key--enter'
         } else {
             return ''
         }
+
     }
     return (
-        <button className={'calculator__key' + addClass()} >{props.operator}</button>
+        <button className={'calculator__key' + addClass()} onClick={(e) => outputHandler(e)}>{operator}</button>
     );
 }
 export default Button
