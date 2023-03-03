ddfc1b538dbc16f7e0f70b46d30e9cc2cae73bec
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-03-03 09:50:54 +0200

    starts todo

--- react/FetchLoops/todolist/src/App.js
+++ react/FetchLoops/todolist/src/App.js
@@ -1,17 +1,9 @@
 import { useState, useEffect } from 'react'
 
 function App() {
-  let [demoData, setDemoData] = useState([]);
-  useEffect(() => {
-    fetch('http://localhost:3000/remoteData.json')
-      .then(res => res.json())
-      .then(data => setDemoData(data.widget))
-  }, [])
-  console.log(demoData.debug);
+ 
   return (
-    <div>
-      <p>{demoData.debug}</p>
-    </div>
+   <p>under construction</p>
   );
 }
 
