32678730224be049131996bf8cc949954b3e9e94
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-03-05 19:08:55 +0200

    todo work

--- react/FetchLoops/todolist/src/components/ToDoList.js
+++ react/FetchLoops/todolist/src/components/ToDoList.js
@@ -4,7 +4,18 @@ const ToDoList = ({ todos, setTodos }) => {
 
     const toggleStatus = (id) => {
         setTodos(state => state.map(c => c.id === id ? ({ ...c, isCompleted: !c.isCompleted }) : c))
-
+    }
+    function delHandler(id) {
+        fetch('http://localhost:3030/jsonstore/todos/' + id, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(res => {
+                setTodos(state => state.filter(x => (x._id !== res._id)))
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
     }
     return (
         <table className="table">
@@ -16,7 +27,7 @@ const ToDoList = ({ todos, setTodos }) => {
                 </tr>
             </thead>
             <tbody>
-                {todos.map(todo => <Todo key={todo._id} {...todo} toggleStatus={toggleStatus} setTodos={setTodos} todos />)}
+                {todos.map(todo => <Todo key={todo._id} {...todo} toggleStatus={toggleStatus} delHandler={delHandler} />)}
             </tbody>
         </table>
     );
--- react/FetchLoops/todolist/src/components/Todo.js
+++ react/FetchLoops/todolist/src/components/Todo.js
@@ -1,14 +1,5 @@
 import styles from './Todo.module.css'
-const Todo = ({ _id, text, isCompleted, toggleStatus, setTodos, todos }) => {
-    function delHandler(id) {
-        fetch('http://localhost:3030/jsonstore/todos/' + id, {
-            method: 'DELETE'
-        })
-            .catch((err) => {
-                console.log(err.message)
-            })
-
-    }
+const Todo = ({ _id, text, isCompleted, toggleStatus, delHandler }) => {
 
     return (
         <tr className={`todo ${(isCompleted ? 'is-completed' : '')}`.trim()} >
@@ -17,7 +8,6 @@ const Todo = ({ _id, text, isCompleted, toggleStatus, setTodos, todos }) => {
             <td className="todo-action">
                 <button className="btn todo-btn" onClick={() => { toggleStatus(_id) }}>Change status</button>
                 <button className={styles['todo-btn']} onClick={() => { delHandler(_id) }}>Del</button>
-
             </td>
         </tr>
     );
