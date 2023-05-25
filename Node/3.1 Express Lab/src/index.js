94745472d5020ca315b853aa7dfc1e6f96a22993
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-05-25 22:01:06 +0300

    fiixed handlebars, seted assets

--- Node/3.1 Express Lab/src/index.js
+++ Node/3.1 Express Lab/src/index.js
@@ -4,17 +4,19 @@ const handlebars = require('express-handlebars')
 const app = express()
 const PORT = 5000
 
+//express config
+app.use(express.static('src/assets'))
+
 //config handlebars
 app.engine('hbs', handlebars.engine({
-    extname: 'hbs'
+    extname: 'hbs',
 }))
 app.set('view engine', 'hbs')
 app.set('views', 'src/views') //hbs serach in root/views by default.
 
 //Routes 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello express</h1>')
+   res.render('home')
 
 })
-
 app.listen(PORT, () => { console.log(`Server listening on port ${PORT}...`) })
