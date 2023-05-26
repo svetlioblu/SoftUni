fd70910ac8d1875a6f0d30e4779d9b32045622f1
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-05-26 07:35:35 +0300

    added bodyparser

--- Node/3.1 Express Lab/src/config/expressConfig.js
+++ Node/3.1 Express Lab/src/config/expressConfig.js
@@ -1,7 +1,9 @@
 const express = require('express')
 
 function expressConfig(app) {
-    //express config
+    //express config to get static files
     app.use(express.static('src/assets'))
+    // express config middlewear to parse post requests as object
+    app.use(express.urlencoded({ extended: false }))
 }
 module.exports = expressConfig
