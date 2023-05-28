84dee5a1e4aaa7617917adb40d8e0b7eecf98348
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-05-28 09:49:16 +0300

    Did range search from- to

--- Node/3.1 Express Lab/src/managers/cubeManager.js
+++ Node/3.1 Express Lab/src/managers/cubeManager.js
@@ -6,9 +6,17 @@ const cubes = []
 //named export
 exports.getAll = (search, from, to) => {
     let result = cubes.slice()
+
     if (search) {
         result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()))
     }
+    if (from) {
+        result = result.filter(cube => cube.difficultyLevel >= Number(from))
+    }
+    if (to) {
+        result = result.filter(cube => cube.difficultyLevel <= Number(to))
+    }
+
     return result
 }
 exports.getOne = (cubeId) => cubes.slice().find(x => x.id === cubeId)
