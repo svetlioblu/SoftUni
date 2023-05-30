de6254bf9941b956f1be76d7c3aa6e6e3092cfb2
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-05-30 07:25:01 +0300

    Did Read Mongoose operations

--- Node/4 NoSQL and MongoDB/mongoose/index.js
+++ Node/4 NoSQL and MongoDB/mongoose/index.js
@@ -4,12 +4,26 @@ const User = require('./models/USER')
 
 async function connnectDb() {
     await mongoose.connect('mongodb://127.0.0.1:27017/user-list')
-
     console.log('DB connected successfuly !')
 
-    const users = await User.find({ firstName: 'Predator' })
+    //Read
+    const allUsers = await User.find()
+    const aFirstUser = await User.findOne()
+    const userById = await User.findById('640f3c9d8ede2e2d040c8a08')
+    const filteredUsers = await User.find({ firstName: 'Predator' })
+    const filteredAUser = await User.findOne({ firstName: 'Predator' })
+
+    //Create
+    
+
+
     // can log users or to call method defined into a model
-    users.forEach(user => user.greet())
+    //=> users.forEach(user => user.greet())
+    //virtual property defind into the model
+    //=> users.forEach(user => console.log(user.info))
+
+
+    console.log(allUsers)
 
 }
 
--- Node/4 NoSQL and MongoDB/mongoose/models/User.js
+++ Node/4 NoSQL and MongoDB/mongoose/models/User.js
@@ -4,7 +4,9 @@ const userSchema = new mongoose.Schema({
     isDeleted: Boolean,
     firstName: {
         type: String,
-        required: true
+        required: true,
+        minLength: 3,
+        maxLength: 12
     },
     lastName: String,
     email: String,
@@ -16,6 +18,10 @@ const userSchema = new mongoose.Schema({
 userSchema.methods.greet = function () {
     console.log(`Hi I am ${this.lastName}`)
 }
+// can create a virtual property that not exists into the db
+userSchema.virtual('info').get(function () {
+    return `My name is ${this.firstName}  ${this.lastName}`
+})
 
 const User = mongoose.model('User', userSchema)
 
