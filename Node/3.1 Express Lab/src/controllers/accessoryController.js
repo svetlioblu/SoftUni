d017cef3773efa8a926b2dc9a8db036f45cbcd94
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-02 07:39:33 +0300

    Created Accessory model

--- Node/3.1 Express Lab/src/controllers/accessoryController.js
+++ Node/3.1 Express Lab/src/controllers/accessoryController.js
@@ -4,4 +4,10 @@ router.get('/create', (rec, res) => {
     res.render('accessory/create')
 })
 
+router.post('/create', (req, res) => {
+    const { name, description, imageUrl } = req.body
+
+    res.redirect('/')
+})
+
 module.exports = router 
---
+++ Node/3.1 Express Lab/src/managers/accessoryManager.js
@@ -0,0 +1,5 @@
+const Accessory = require('../models/Accessory')
+
+exports.create(accessoryData) => {
+
+}
