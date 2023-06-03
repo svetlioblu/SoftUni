0d55d396a38274e3d8aec100a16d3b0ccbf1fca5
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-03 22:46:08 +0300

    added back path in attach.hbs

--- Node/3.1 Express Lab/src/managers/accessoryManager.js
+++ Node/3.1 Express Lab/src/managers/accessoryManager.js
@@ -5,3 +5,5 @@ exports.create = (accessoryData) => Accessory.create(accessoryData)
 exports.getAll = () => Accessory.find()
 
 exports.getAvailable = (accessories) => Accessory.find({ _id: { $nin: accessories} })
+
+
