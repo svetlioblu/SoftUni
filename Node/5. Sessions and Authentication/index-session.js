6715ce935dbb271d17d2ca5a4a8bb4cc80df4598
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-05 08:35:17 +0300

    practice sessions finished

--- Node/5. Sessions and Authentication/index-session.js
+++ Node/5. Sessions and Authentication/index-session.js
@@ -3,24 +3,24 @@ const app = express()
 const { v4: uuid } = require('uuid')
 const cookieParser = require('cookie-parser')
 
-//use middleware
 app.use(cookieParser())
 
 app.get('/', (req, res) => {
     let id = uuid()
-    //get cookie
+    const session = {}
+
     const userId = req.cookies['userId']
 
     if (userId) {
         id = userId
     } else {
+        //session is info stored into the server memmory, not retun to the client there npm
+        session[id] = {
+            secret: 'mySecret'
+        }
+        // creates cookie if no such
         res.cookie('userId', id, { httpOnly: true })
     }
     res.send('holla  ' + id)
 })
-
-app.get('/login', (req, res) => {
-
-})
-
 app.listen(5000, () => { console.log('Server listening on Port 5000...') })
