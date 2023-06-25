c753303b1ebebed2426f0a0612430e8cde68d550
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-25 17:37:18 +0300

    exersise

--- AngularJs/2.Components/component-demo-app/src/app/app.component.ts
+++ AngularJs/2.Components/component-demo-app/src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'App component';
+  
+  onOutputFromChild(){
+console.log('onOutputFromChild');
+  }
 }
--- AngularJs/2.Components/component-demo-app/src/app/navigation/navigation.component.ts
+++ AngularJs/2.Components/component-demo-app/src/app/navigation/navigation.component.ts
@@ -1,21 +1,39 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  AfterViewInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css'],
 })
-export class NavigationComponent {
-  title = 'hide';
+export class NavigationComponent implements OnInit, AfterViewInit {
+  // inpit comes from parent parentName i qots
+  @Input('titleFromApp') titleFromApp: string = '';
+  @Output() onChildOutput = new EventEmitter<boolean>();
   isActive = false;
+  inputValue = 'Hello';
   activeUsers = [
     { name: 'Mitko', age: 21 },
     { name: 'Ivan', age: 23 },
     { name: 'Toni', age: 33 },
     { name: 'Goro', age: 62 },
   ];
-    
-  handleClick(): void {
+  // spred of number arguments
+  handleClick(event: Event, ...args: number[]): void {
     this.isActive = !this.isActive;
+    this.onChildOutput.emit(this.isActive); 
+  }
+  //lifecycles
+  ngOnInit() {
+    console.log('ON INIt!!');
+  }
+  ngAfterViewInit() {
+    console.log('after view init!!');
   }
 }
