4a35dcb535db133e849aca46b790d1266841c296
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-25 10:53:56 +0300

    add navigation component

--- AngularJs/2.Components/component-demo-app/src/app/app.module.ts
+++ AngularJs/2.Components/component-demo-app/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, NavigationComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
