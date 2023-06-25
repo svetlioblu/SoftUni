88f04be2dc8328d4f3130d77d4e00d5e5bf2c030
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-25 10:24:38 +0300

    deleted auto components

--- AngularJs/2.Components/component-demo-app/src/app/app.module.ts
+++ AngularJs/2.Components/component-demo-app/src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
