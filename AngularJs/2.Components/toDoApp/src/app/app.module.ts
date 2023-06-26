cd148b5bc503a758569055cfda45b01f3886e4e6
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-26 07:12:41 +0300

    add list and item component

--- AngularJs/2.Components/toDoApp/src/app/app.module.ts
+++ AngularJs/2.Components/toDoApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
