71ac4e256f7365a5434c905bb9c7e6c1b1f74567
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-06-29 07:54:21 +0300

    added service

--- AngularJs/3.RXJS and Services/RxDemoApp/src/app/app.component.ts
+++ AngularJs/3.RXJS and Services/RxDemoApp/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'RxDemoApp';
+//injection from the service
+  constructor(public userService:UserService) {}
 }
