71650aa70d8f97efedf5efd9ffedd582b23c2a81
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-09 08:42:05 +0300

    add navigation component

--- AngularJs/4.Modules and Routing/Routing/src/app/core/core.module.ts
+++ AngularJs/4.Modules and Routing/Routing/src/app/core/core.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GlobalLoaderComponent } from './global-loader/global-loader.component';
 import { SharedModule } from '../shared/shared.module';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
-  declarations: [GlobalLoaderComponent],
+  declarations: [GlobalLoaderComponent, NavigationComponent],
   imports: [CommonModule, SharedModule],
   exports: [GlobalLoaderComponent],
 })
