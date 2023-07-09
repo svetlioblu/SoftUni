98f6fc10edbaeb7970f9d61c53373f2562ac6f49
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-09 09:04:51 +0300

    update navigation

--- AngularJs/4.Modules and Routing/Routing/src/app/core/navigation/navigation.component.ts
+++ AngularJs/4.Modules and Routing/Routing/src/app/core/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
-
+  // 1. inject router
+  constructor(private router: Router) { }
+  //2.config router, use navigateTo in btns
+  navigateTo(path: string): void {
+    this.router.navigate([path])
+  }
 }
