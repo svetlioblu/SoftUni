4f5fbdb40aee1450834b56eb8664a1ff3075c05c
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-10 07:06:15 +0300

    update user guard

--- AngularJs/4.Modules and Routing/Routing/src/app/user/user-details/user-details.guard.ts
+++ AngularJs/4.Modules and Routing/Routing/src/app/user/user-details/user-details.guard.ts
@@ -11,11 +11,13 @@ export class AuthGuard implements CanActivate {
     constructor(private router: Router) { }
 //3. add built-in method
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        return this.checkIfLogged(state.url)
+       // where to redirect in urlTree if checkIg Logged failed 
+        return this.checkIfLogged(state.url) || this.router.createUrlTree(['/user-list'])
     }
 
     // 4. add custom method to check what condition is needed
     checkIfLogged(url:string): boolean {
-        return false
+        //if return false will protect the path
+        return true
     }
 }
