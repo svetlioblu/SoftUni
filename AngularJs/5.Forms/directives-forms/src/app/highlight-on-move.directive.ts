c3f75ee4b6f3d115e6065b1f7552b8bcf5c7e91f
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-16 07:38:46 +0300

    add directive listeners

--- AngularJs/5.Forms/directives-forms/src/app/highlight-on-move.directive.ts
+++ AngularJs/5.Forms/directives-forms/src/app/highlight-on-move.directive.ts
@@ -1,10 +1,38 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightOnMove]'
 })
-export class HighlightOnMoveDirective {
+export class HighlightOnMoveDirective implements OnInit {
 
-  constructor() { }
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+
+  ngOnInit(): void {
+    // this.elRef.nativeElement.style.backgroundColor = 'yellow'
+
+    this.renderer.setStyle(
+      this.elRef.nativeElement, 'background-color', 'red'
+    )
+
+    this.renderer.listen(
+      this.elRef.nativeElement,
+      'mouseenter',
+      this.mouseEnterHandler
+    )
+    this.renderer.listen(
+      this.elRef.nativeElement,
+      'mouseleave',
+      this.mouseLeaveHandler
+    )
+  }
+  mouseEnterHandler(e: MouseEvent) {
+    console.log('enter', e);
+
+  }
+  mouseLeaveHandler(e: MouseEvent) {
+    console.log('leave', e);
+
+  }
 
 }
