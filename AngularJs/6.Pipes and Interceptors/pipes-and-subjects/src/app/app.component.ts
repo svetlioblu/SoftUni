3254878d4bbce34486ace172b265d1e3c8fb2685
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-23 07:44:52 +0300

    add onInit in app component

--- AngularJs/6.Pipes and Interceptors/pipes-and-subjects/src/app/app.component.ts
+++ AngularJs/6.Pipes and Interceptors/pipes-and-subjects/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   user = { name: 'Pesho', age: 12, list: [1, 2, 3, 4, 5, 6, 7, 8] }
   p = Promise.resolve(100)
   sum(a: number, b: number): number {
@@ -14,6 +15,14 @@ export class AppComponent {
 
   addProperty(): void {
     (this.user as any)['test123'] = 'test123'
+  }
+
+  constructor(private userService: UserService) { }
+  ngOnInit(): void {
+    this.userService.loadUsers().subscribe(
+      
+      
+    )
   }
 
 }
