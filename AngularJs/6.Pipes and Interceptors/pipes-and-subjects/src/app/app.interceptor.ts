5d339107ef595a2e76ddb77246496a4ac80af925
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-23 06:58:52 +0300

    add interceptor

--- AngularJs/6.Pipes and Interceptors/pipes-and-subjects/src/app/app.interceptor.ts
+++ AngularJs/6.Pipes and Interceptors/pipes-and-subjects/src/app/app.interceptor.ts
@@ -1,9 +1,23 @@
 
-import { HttpInterceptor } from '@angular/common/http'
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http'
+import { Observable, tap } from 'rxjs';
+import { API_URL } from './constants';
 
 export class AppInterceptor implements HttpInterceptor {
 
-    intercept() {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let request = req
 
+        if (req.url.startsWith('/api')) {
+            request = request.clone({
+                url: req.url.replace('/api', API_URL)
+            })
+        }
+
+        return next.handle(request).pipe(
+            tap(
+                (req) => console.log(req)
+            )
+        )
     }
 }
