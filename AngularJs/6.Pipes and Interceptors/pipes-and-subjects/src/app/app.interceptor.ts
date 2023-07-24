6dd56804f5ea2424457794cfc83e699a3a4a9c6f
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-24 07:51:27 +0300

    add Error handling Interceptor

--- AngularJs/6.Pipes and Interceptors/pipes-and-subjects/src/app/app.interceptor.ts
+++ AngularJs/6.Pipes and Interceptors/pipes-and-subjects/src/app/app.interceptor.ts
@@ -1,6 +1,6 @@
 
 import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http'
-import { Observable, tap } from 'rxjs';
+import { EMPTY, Observable, catchError, tap } from 'rxjs';
 import { API_URL } from './constants';
 import { Provider } from '@angular/core';
 
@@ -18,11 +18,19 @@ export class AppInterceptor implements HttpInterceptor {
         return next.handle(request).pipe(
             tap(
                 (req) => console.log(req)
-            )
+            ),
+            // can catch all errors. instead of uzing component Observable error
+            catchError((err) => {
+                if (err.status === 0) {
+                    console.error(`Error from Interceptor: ${JSON.stringify(err)}`);
+                    return EMPTY
+                }
+                return [err]
+            })
         )
     }
 }
-// set Interceptor to be providable in app.module providers !
+// ! set Interceptor to be providable in app.module providers 
 export const appInterceptorProvider: Provider = {
     provide: HTTP_INTERCEPTORS,
     multi: true,
