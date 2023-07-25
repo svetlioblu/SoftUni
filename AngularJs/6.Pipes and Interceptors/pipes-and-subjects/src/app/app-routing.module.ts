74ffeb201579213a31f78613e79f145d75e6db51
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-25 21:13:28 +0300

    add ** path in app-routing

--- AngularJs/6.Pipes and Interceptors/pipes-and-subjects/src/app/app-routing.module.ts
+++ AngularJs/6.Pipes and Interceptors/pipes-and-subjects/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NoFound404Component } from './no-found404/no-found404.component';
 
 const routes: Routes = [
   {
@@ -12,13 +13,16 @@ const routes: Routes = [
   {
     path: 'lazy',
     loadChildren: () => import('./lazy/lazy.module').then((m) => m.LazyModule)
-  }
+  },
+  { path: '**', component: NoFound404Component }
+
 ];
 
-// can define preloadingStrategy
+// can define preloadingStrategy, enableTracing and ...
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    //preloadingStrategy: PreloadAllModules
+    //preloadingStrategy: PreloadAllModules",
+    //enableTracing:true -> to debug redirect and routing in general
   })],
   exports: [RouterModule]
 })
