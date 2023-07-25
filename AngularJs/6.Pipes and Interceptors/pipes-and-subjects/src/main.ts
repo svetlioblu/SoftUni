34562c0e3aa7531649d43302d06b2bd40f9d0808
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-25 21:13:41 +0300

    add subjects

--- AngularJs/6.Pipes and Interceptors/pipes-and-subjects/src/main.ts
+++ AngularJs/6.Pipes and Interceptors/pipes-and-subjects/src/main.ts
@@ -1,7 +1,25 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { Subject } from 'rxjs';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+//! SUBJECTS
+
+const subj$$ = new Subject()
+subj$$.subscribe(console.log) //A
+subj$$.next(123) //A
+subj$$.subscribe(console.log) //B
+subj$$.subscribe(console.log) //C
+subj$$.next(123) //ABC
+
+subj$$.subscribe({
+  next: console.log,
+  error: (err) => console.log(err),
+  complete: () => console.log('completed')
+
+}
+)
