ce1448023c4744d33b281895cfc91438f47dd6ec
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-25 21:12:35 +0300

    add 404 component

---
+++ AngularJs/6.Pipes and Interceptors/pipes-and-subjects/src/app/no-found404/no-found404.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-found404',
+  templateUrl: './no-found404.component.html',
+  styleUrls: ['./no-found404.component.css']
+})
+export class NoFound404Component {
+
+}
