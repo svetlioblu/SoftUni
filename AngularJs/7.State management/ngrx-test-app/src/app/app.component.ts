c9e0efc62e7a154de063ef4b767f3677c3424dbf
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-09-09 11:34:24 +0300

    demo state app

--- AngularJs/7.State management/ngrx-test-app/src/app/app.component.ts
+++ AngularJs/7.State management/ngrx-test-app/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngrx-test-app';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.demoTest()
+  }
+
+  demoTest() { }
+  // Redux -> design pattern => single source of true =>state
+  // we dont chande the state directly, but dispatch an action
+  // Reducer capters the action and do something
+
 }
